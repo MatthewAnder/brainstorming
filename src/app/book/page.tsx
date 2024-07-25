914c1b5e5a5846d85641bc96b41a79fc47bdc154
Matthew Haryanto <matthewanh@hotmail.com>
2024-07-25 14:50:33 -0700

    feat: add book page

---
+++ src/app/book/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Box, Flex, Heading } from "@chakra-ui/react";
+
+import { useCallback, useState } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+import type { PDFDocumentProxy } from "pdfjs-dist";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+const options = {
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
+};
+
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type PDFFile = string | File | null;
+
+export default function Sample() {
+  const [numPages, setNumPages] = useState<number>();
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <Box>
+      <header
+        style={{
+          padding: "20px",
+          borderBottom: "2px solid #353534",
+          borderColor: "border",
+        }}
+      >
+        <Heading fontSize={"lg"}>react-pdf sample page</Heading>
+      </header>
+      <Flex
+        flexDir={"column"}
+        alignItems={"center"}
+        p={5}
+        mx={5}
+        ref={setContainerRef}
+      >
+        <Document
+          file={"example.pdf"}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={
+                containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+              }
+            />
+          ))}
+        </Document>
+      </Flex>
+    </Box>
+  );
+}
