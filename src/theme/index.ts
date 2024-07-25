2ab141d71d2ea5e71ebfc87d01ef7eb4eac62ab5
Matthew Haryanto <matthewanh@hotmail.com>
2024-07-24 17:16:42 -0700

    feat: add heading and text font

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,21 @@
 import { extendTheme, Heading } from "@chakra-ui/react";
 import colors from "./colors";
+import { Open_Sans, PT_Sans } from "next/font/google";
+
+const headingFont = Open_Sans({
+  subsets: ["latin"],
+});
+const bodyFont = PT_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const theme = extendTheme({
   colors,
+  fonts: {
+    heading: headingFont.style.fontFamily,
+    body: bodyFont.style.fontFamily,  
+  },
   styles: {
     global: {
       "html, body": {
