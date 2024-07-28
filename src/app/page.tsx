3aad6ea579f83db80fd26324d23e36534a299894
Matthew Haryanto <matthewanh@hotmail.com>
2024-07-28 00:37:55 -0700

    feat: add avatar with setting icon button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
-import { redirect } from "next/navigation";
-import { validateRequest } from "@/lib/auth";
 
-export default async function Home() {
-  const { user } = await validateRequest();
-  if (!user) {
-    return redirect("/login");
-  }
-
-  return (
-    <Box>
-      <Text>Hello {user.username}!</Text>
-    </Box>
-  );
+export default function Home() {
+  return;
 }
